e36e145a5484dfc83ef82a1165acb62f514516fa
Adrian Martinez <jadrianmtzglez@gmail.com>
2026-04-15 21:06:10 -0400

    Auth routes

--- final_project/index.js
+++ final_project/index.js
@@ -12,7 +12,7 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
-    if (req.session.authorization)
+    if (!req.session.authorization)
         return res.status(403).json({ message: "User not logged in" });
 
     const { authToken } = req.session.authorization;
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -11,19 +11,60 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    //write code to check if username and password match the one we have in records.
+    return users.filter(user => user.username === username && user.password === password).length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const { username, password } = req.body;
+
+    if (!username || !password)
+        return res.status(404).json({ message: "Username and/or password are missing" });
+    if (!authenticatedUser(username, password))
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+
+    // Generate JWT access token
+    let authToken = jwt.sign({
+        data: username
+    }, 'jwtSecret', { expiresIn: 60 * 60 });
+
+    // Store access token and username in session
+    req.session.authorization = {
+        authToken, username
+    }
+    return res.status(200).json({message: "User successfully logged in"});
 });
 
-// Add a book review
+// Add/modify a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const { isbn } = req.params;
+    const { review } = req.query;
+
+    if (!review)
+        return res.status(404).json({message: "Unable to add review."});
+
+    const { username } = req.session.authorization;
+    let { reviews } = books[isbn];
+    reviews[username] = review;
+    books[isbn].reviews = reviews;
+
+    return res.status(200).json({message: "Review added successfully"});
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    //Write your code here
+    const { isbn } = req.params;
+
+    const { username } = req.session.authorization;
+    let { reviews } = books[isbn];
+    delete reviews[username];
+    books[isbn].reviews = reviews;
+
+    return res.status(200).json({message: "Review deleted successfully"});
 });
 
 module.exports.authenticated = regd_users;
